efaedd5dd103400ca61ab142dc224e2794a0b1f1
Yashar <faalian.m@gmail.com>
2023-02-05 17:18:26 +0330

    feat stream gorupby and match

--- src/main/java/streams/_Stream.java
+++ src/main/java/streams/_Stream.java
@@ -1,6 +1,11 @@
 package streams;
 
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static streams._Stream.Sex.FEMALE;
@@ -12,15 +17,34 @@ public class _Stream {
         List<Person> people = List.of(
                 new Person("Mohammad", "Faalian", 34, MALE),
                 new Person("Amirali", "Nejad", 8, MALE),
-                new Person("Mahboub", "Tabasi", 29, FEMALE)
+                new Person("Mahboub", "Tabasi", 29, FEMALE),
+                new Person("Armin", "Nejadi", 8, MALE)
         );
+
+        people.stream()
+                //.filter(person -> person.sex.equals(FEMALE))
+                .sorted(Comparator.comparing(Person::getAge))
+                        .forEach(System.out::println);
 
         people.stream()
                 .map(person -> person.age)
                 .forEach(System.out::println);
 
+        System.out.println(people.stream().allMatch(person -> person.age < 40));
+
+        Optional<Person> max = people.stream()
+                .min(Comparator.comparing(Person::getAge));
+
+        max.ifPresent(System.out::println);
+
+        people.stream()
+                .collect(Collectors.groupingBy(Person::getAge))
+                .forEach((integer, people1) -> System.out.println(integer + " " + people1));
+
     }
 
+    @Getter
+    @ToString
     static class Person {
         private String name;
         private String lastName;
